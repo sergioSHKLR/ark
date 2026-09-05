66dee3961b98cc0828c8c7ce5deb10e4c5756783
sergioSHKLR <sergio@shklr.org>
2026-09-05 20:27:07 -0300

    Add Noah Protocol PWA with dove icon
    Local daily rite (morning, day, night, log), EN/PT, chant desk, and
    installable shell with parchment dove icons. Ignore local audio dumps.

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_VERSION = "noah-protocol-v11";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/favicon.svg",
+  "./icons/favicon.ico",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable-512.png",
+  "./icons/apple-touch-icon.png",
+  "./fonts/eb-garamond-400.woff2",
+  "./fonts/eb-garamond-400-italic.woff2",
+  "./fonts/eb-garamond-700.woff2",
+  "./fonts/unifrakturmaguntia-400.woff2",
+];
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches
+      .open(CACHE_VERSION)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting()),
+  );
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.map((k) => {
+            if (k !== CACHE_VERSION) return caches.delete(k);
+          }),
+        ),
+      )
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (evt) => {
+  const url = new URL(evt.request.url);
+  if (url.origin !== self.location.origin) return;
+  evt.respondWith(
+    fetch(evt.request)
+      .then((resp) => {
+        if (resp && resp.ok && evt.request.method === "GET") {
+          const clone = resp.clone();
+          caches.open(CACHE_VERSION).then((cache) => cache.put(evt.request, clone));
+        }
+        return resp;
+      })
+      .catch(() =>
+        caches
+          .match(evt.request)
+          .then((cached) => cached || caches.match("./index.html")),
+      ),
+  );
+});
